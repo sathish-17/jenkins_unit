c409108eaf79256060c39d7e371e1934123b664e
sathish <sathishsharma72@gail.com>
2023-02-08 00:31:08 +0530

    initial coit

---
+++ code/jenkins_test.py
@@ -0,0 +1,11 @@
+def ad(a,b):
+    if a<b:
+        c = a+b
+    else:
+        c = a-b
+    return c
+
+x = int(input("enter a number: "))
+y = int(input("enter a number: "))
+res = ad(x,y)
+print(res)
